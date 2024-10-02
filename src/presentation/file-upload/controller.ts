5ae1743ab92045934c559a07ce964e2118af894f
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-02 00:02:49 -0500

    add custom fileUpload middleware

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -25,14 +25,22 @@ export class FileUploadController {
 
     uploadFile = (req: Request, res: Response) => {
 
-        const files = req.files;
+        const type = req.params.type;
+
+        const validTypes = ['users', 'products', 'categories'];
+
+        if (!validTypes.includes(type)) {
+            return res.status(400).json({ error: `Invalid type parameter: Only ${validTypes}` });
+        }
+
+
         if (!req.files || Object.keys(req.files).length === 0) {
             return res.status(400).json({ error: 'No files were uploaded.' });
         }
 
-        const file = req.files.file;
+        const file = req.body.files.at(0)  as UploadedFile;
 
-        this.fileUploadService.uploadSingle(file as UploadedFile)
+        this.fileUploadService.uploadSingle(file, `uploads/${type}`)
             .then((uploaded) => res.json(uploaded))
             .catch((error) => this.handleError(error, res));
 
--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { FileUploadController } from './controller';
 import { FileUploadService } from '../services/file-upload.service';
+import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
 
 
 
@@ -16,6 +17,7 @@ export class FileUploadRoutes {
     const fileUploadService= new FileUploadService();
     const controller = new FileUploadController(fileUploadService);
     
+    router.use(FileUploadMiddleware.containFiles);
     // Definir las rutas
     // router.use('/api/todos', /*TodoRoutes.routes */ );
     router.post('/single/:type', controller.uploadFile);
