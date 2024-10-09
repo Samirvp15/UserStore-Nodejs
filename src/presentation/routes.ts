61a7e8d3ae428880d73be9bbc896cd5b58b027b0
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-08 22:59:02 -0500

    add routes and controller for Tickets

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,6 +3,7 @@ import { AuthRoutes } from './auth/routes';
 import { CategoryRoutes } from './category/routes';
 import { ProductsRoutes } from './products/routes';
 import { FileUploadRoutes } from './file-upload/routes';
+import { TicketRoutes } from './tickets/routes';
 
 
 
@@ -16,10 +17,12 @@ export class AppRoutes {
     
     // Definir las rutas
     // router.use('/api/todos', /*TodoRoutes.routes */ );
-    router.use('/api/auth', AuthRoutes.routes);
-    router.use('/api/categories', CategoryRoutes.routes);
-    router.use('/api/products', ProductsRoutes.routes);
-    router.use('/api/upload', FileUploadRoutes.routes);
+    // router.use('/api/auth', AuthRoutes.routes);
+    // router.use('/api/categories', CategoryRoutes.routes);
+    // router.use('/api/products', ProductsRoutes.routes);
+    // router.use('/api/upload', FileUploadRoutes.routes);
+
+    router.use('/api/ticket', TicketRoutes.routes);
 
 
 
